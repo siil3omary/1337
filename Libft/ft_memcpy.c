172535c48d2188f94341bb48336723f87cfb3fdd
L3OMARY <L3OMARY@ICLOUD.COM>
2023-09-23 19:07:02 +0100

    memcpy

---
+++ Libft/ft_memcpy.c
@@ -0,0 +1,15 @@
+#include "libft.h"
+void    *ft_memcpy(void *dst, const void *src, size_t n){
+unsigned char *s = (unsigned char *)src;
+unsigned char *d = (unsigned char *)dst;
+int i;
+i = 0;
+
+while (i < n)
+{
+  d[i]=s[i];
+  i++;
+}
+return dst;
+
+}
