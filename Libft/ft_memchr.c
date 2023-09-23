17e15f0f6b4b190384f92689e019c1dced08858e
L3OMARY <L3OMARY@ICLOUD.COM>
2023-09-23 23:47:06 +0100

    done !

---
+++ Libft/ft_memchr.c
@@ -0,0 +1,20 @@
+#include "libft.h"
+void    *ft_memchr(const void *s, int c, size_t n){
+    unsigned char *se = (unsigned char *)s;
+
+
+int i;
+i = 0;
+  
+while (i < n)
+{
+    if(se[i] == c){
+        return &se[i]; 
+
+    }
+    i++;
+}
+return NULL;
+
+
+}
---
+++ Libft/ft_memmove.c
@@ -0,0 +1,26 @@
+#include "libft.h"
+#include <stdio.h>
+#include <string.h>
+void    *ft_memmove(void *dst, const void *src, size_t n){
+    unsigned char *s = (unsigned char *)src;
+unsigned char *d = (unsigned char *)dst;
+int i;
+i = 0;
+    if(d > s){
+        while (n-- > 0)
+        {
+            d[n] = s[n];
+        }
+        
+
+    }
+    else {
+        while (i < n)
+{
+  d[i]=s[i];
+  i++;
+}
+    }
+    return dst;
+}
+
