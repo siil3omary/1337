4e197287824e112e87be81b91ecc41a779b14606
L3OMARY <L3OMARY@ICLOUD.COM>
2023-09-23 17:38:43 +0100

    change

---
+++ Libft/ft_memset.c
@@ -0,0 +1,17 @@
+#include <string.h>
+
+void *ft_memset(void *b, int c, size_t len){
+   int i = 0;
+       char *s = (char*) b;
+       while (i < len)
+       {
+     s[i++] = c;
+       }
+       
+
+return b;
+    
+}
+
+
+
