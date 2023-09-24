dd9744cee7565adcb5297931ffeeda514e6ed510
L3OMARY <L3OMARY@ICLOUD.COM>
2023-09-24 02:44:32 +0100

    Done !!

---
+++ Libft/ft_strdup.c
@@ -0,0 +1,16 @@
+#include "libft.h"
+#include <stdlib.h>
+char    *ft_strdup(const char *s1){
+char *dst;
+    if(!( dst = (char)malloc(sizeof(char) * (ft_strlen(s1) +1)))){
+        return NULL;
+    }
+    int i = 0;
+    while (s1[i])
+    {
+        dst[i] = s1[i];
+        i++;
+    }
+    dst[i] = '\0';
+    return dst;
+}
